81d6778bb4d2ef763753ca32de2c3989b200b5d6
Toffer305 <toffer305@gmail.com>
2022-12-07 22:23:38 -0500

    finalized translation logic

--- final_project/server.py
+++ final_project/server.py
@@ -8,13 +8,15 @@ app = Flask("Web Translator")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to French"
+    translated_text = translator.english_to_french(textToTranslate)
+    return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
     # Write your code here
-    return "Translated text to English"
+    translated_text = translator.french_to_english(textToTranslate)
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
